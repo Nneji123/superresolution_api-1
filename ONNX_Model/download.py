49135595ff65bb5486a358cb098a955f0a8b3d4d
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2022-08-13 17:52:56 +0000

    updated files

--- ONNX_Model/download.py
+++ ONNX_Model/download.py
@@ -6,6 +6,11 @@ def url_to_id(url):
 
 id = url_to_id('https://drive.google.com/file/d/1ms1-uVh3VOVVlfdPWbJ8Luei3c4IJJ_J/view?usp=sharing')
 
-url = 'https://drive.google.com/uc?id='+id
-output = 'model.onnx'
-gdown.download(url, output, quiet=False)
+
+def main():
+    url = 'https://drive.google.com/uc?id='+id
+    output = 'model.onnx'
+    gdown.download(url, output, quiet=False)
+
+if __name__=="__main__":
+    main()
